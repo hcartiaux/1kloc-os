6244a78aa6638228f7d537b079175828fe2c4982
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2025-06-16 21:38:38 +0200

    [07. Kernel Panic] Section completed

--- kernel.c
+++ kernel.c
@@ -30,9 +30,15 @@ void kernel_main(void) {
     printf("\n\nHello %s\n", "World!");
     printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
 
+    memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
+
+    PANIC("booted!");
+    printf("unreachable here!\n");
+
     for (;;) {
         __asm__ __volatile__("wfi");
     }
+
 }
 
 __attribute__((section(".text.boot")))
