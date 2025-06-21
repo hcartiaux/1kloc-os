55ca7dc949947db64a41690a3e80015c3797d54a
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2025-06-21 14:29:53 +0200

    [10. Process] Testing context switch

--- kernel.h
+++ kernel.h
@@ -1,6 +1,17 @@
 #pragma once
 #include "common.h"
 
+#define PROCS_MAX 8       // Maximum number of processes
+#define PROC_UNUSED   0   // Unused process control structure
+#define PROC_RUNNABLE 1   // Runnable process
+
+struct process {
+    int pid;             // Process ID
+    int state;           // Process state: PROC_UNUSED or PROC_RUNNABLE
+    vaddr_t sp;          // Stack pointer
+    uint8_t stack[8192]; // Kernel stack
+};
+
 struct sbiret {
     long error;
     long value;
